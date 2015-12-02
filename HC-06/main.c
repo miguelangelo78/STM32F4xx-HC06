bde4ea62ca543bfe949d986b424d71aee4dfd814
Miguel <santosmiguel25@gmail.com>
2015-12-02 16:36:49 +0000

    Important comment added

--- HC-06/main.c
+++ HC-06/main.c
@@ -1,11 +1,15 @@
 #include "stm32f4xx_rcc.h"
 #include "hc06.h"
 
+/* NOTE: Somehow, the HC-06 does not respond to STM32F4 when commands are sent.
+ * However, it answers the same commands but from an Arduino, which is annoying.
+ * We will fix this. */
+
 int main(void)
 {
 	/***** IMPORTANT: *****/
 	/* Before running SystemInit(); make the necessary changes as stated in the following URL:
-	 * http://stackoverflow.com/a/21485189  */
+	 * http://stackoverflow.com/a/21485189 */
 	/**********************/
 	SystemInit();
 	SystemCoreClockUpdate();
